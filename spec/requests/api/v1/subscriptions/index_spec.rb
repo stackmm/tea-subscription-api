8ad9681dae54068e7113cdac124ca260f73a4e68
Steven Stackhouse <stackmm@gmail.com>
2023-09-12 17:17:03 -0600

    create GET customer's subscriptions index request test file

---
+++ spec/requests/api/v1/subscriptions/index_spec.rb
@@ -0,0 +1,59 @@
+require "rails_helper"
+
+RSpec.describe "Customer's Subscriptions Index API" do
+  describe "GET /api/v1/customers/:id/subscriptions" do
+    describe "Happy Path" do
+      it "returns a list of all subscriptions for a customer" do
+        customer_1 = create(:customer)
+        customer_2 = create(:customer)
+
+        tea_1 = create(:tea)
+        tea_2 = create(:tea)
+        tea_3 = create(:tea)
+
+        subscription_1 = create(:subscription, customer: customer_1, tea: tea_1)
+        subscription_2 = create(:subscription, customer: customer_1, tea: tea_2)
+        subscription_3 = create(:subscription, customer: customer_1, tea: tea_3)
+        subscription_4 = create(:subscription, customer: customer_2, tea: tea_1)
+        subscription_5 = create(:subscription, customer: customer_2, tea: tea_2)
+
+        get "/api/v1/customers/#{customer_1.id}/subscriptions"
+
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+
+        subscriptions = JSON.parse(response.body, symbolize_names: true)
+
+        expect(subscriptions[:data].count).to eq(3)
+
+        subscriptions[:data].each do |subscription|
+          expect(subscription).to have_key(:id)
+          expect(subscription).to have_key(:type)
+          expect(subscription[:type]).to eq("subscription")
+          expect(subscription).to have_key(:attributes)
+          expect(subscription[:attributes]).to have_key(:title)
+          expect(subscription[:attributes][:title]).to be_a(String)
+          expect(subscription[:attributes]).to have_key(:price)
+          expect(subscription[:attributes][:price]).to be_a(Float)
+          expect(subscription[:attributes]).to have_key(:status)
+          expect(subscription[:attributes][:status]).to be_a(String)
+          expect(subscription[:attributes]).to have_key(:frequency)
+          expect(subscription[:attributes][:frequency]).to be_a(String)
+        end
+
+        get "/api/v1/customers/#{customer_2.id}/subscriptions"
+
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+
+        subscriptions = JSON.parse(response.body, symbolize_names: true)
+
+        expect(subscriptions[:data].count).to eq(2)
+      end
+    end
+
+    describe "Sad Path" do
+
+    end
+  end
+end
