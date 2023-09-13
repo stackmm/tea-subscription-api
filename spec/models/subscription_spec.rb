5503015d562c5cde3a1ffdca0a0fa80e990be911
Steven Stackhouse <stackmm@gmail.com>
2023-09-13 12:58:32 -0600

    add validations for price numericality and frequency types

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -11,5 +11,7 @@ RSpec.describe Subscription, type: :model do
     it { should validate_presence_of :price }
     it { should validate_presence_of :status }
     it { should validate_presence_of :frequency }
+    it { should validate_numericality_of :price }
+    it { should validate_inclusion_of(:frequency).in_array(["Yearly", "Monthly", "Weekly"]) }
   end
 end
