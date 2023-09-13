dbe1268f6ff50eae8060d541f4c9a716c5f3c30b
Steven Stackhouse <stackmm@gmail.com>
2023-09-13 10:56:51 -0600

    move 404 sad path to application controller

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,11 +1,7 @@
 class Api::V1::SubscriptionsController < ApplicationController
   def index
     customer = Customer.find(params[:customer_id])
-
-    if customer
-      render json: SubscriptionSerializer.new(customer.subscriptions)
-    else
-      render json: {error: "Customer not found"}, status: 404
-    end
+    subscriptions = customer.subscriptions
+    render json: SubscriptionSerializer.new(subscriptions)
   end
 end
