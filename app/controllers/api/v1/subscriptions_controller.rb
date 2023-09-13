87dd4545fe9046cd6c7fce06b0f0ea8d62520753
Steven Stackhouse <stackmm@gmail.com>
2023-09-13 12:04:21 -0600

    add create method

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -4,4 +4,14 @@ class Api::V1::SubscriptionsController < ApplicationController
     subscriptions = customer.subscriptions
     render json: SubscriptionSerializer.new(subscriptions)
   end
+
+  def create
+    subscription = Subscription.create!(subscription_params)
+    render json: SubscriptionSerializer.new(subscription), status: 201
+  end
+
+  private
+  def subscription_params
+    params.permit(:title, :price, :frequency, :customer_id, :tea_id)
+  end
 end
