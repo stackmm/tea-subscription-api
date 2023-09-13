e9d15378735f4f4e1da127ba7ea4a09ef20a3316
Steven Stackhouse <stackmm@gmail.com>
2023-09-13 12:40:29 -0600

    add sad path for customer not found

--- spec/requests/api/v1/subscriptions/create_spec.rb
+++ spec/requests/api/v1/subscriptions/create_spec.rb
@@ -23,8 +23,8 @@ RSpec.describe "Create Subscription API" do
         subscription = JSON.parse(response.body, symbolize_names: true)
 
         expect(subscription[:data][:attributes][:title]).to eq(subscription_params[:title])
-        expect(subscription[:data][:attributes][:price]).to eq(subscription_params[:price])
-        expect(subscription[:data][:attributes][:status]).to eq(subscription_params[:status])
+        expect(subscription[:data][:attributes][:price]).to eq(subscription_params[:price].to_s)
+        expect(subscription[:data][:attributes][:status]).to eq("active")
         expect(subscription[:data][:attributes][:frequency]).to eq(subscription_params[:frequency])
         expect(subscription[:data][:attributes][:customer_id]).to eq(subscription_params[:customer_id])
         expect(subscription[:data][:attributes][:tea_id]).to eq(subscription_params[:tea_id])
@@ -32,7 +32,45 @@ RSpec.describe "Create Subscription API" do
     end
 
     describe "Sad Path" do
+      it "returns a 400 error if the customer is not found" do
+        tea = create(:tea)
 
+        subscription_params = {
+          title: "My Monthly Tea Subscription",
+          price: 10.00,
+          frequency: "Monthly",
+          customer_id: 1,
+          tea_id: tea.id
+        }
+
+        post "/api/v1/subscriptions", params: subscription_params
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+        
+        response_json = JSON.parse(response.body, symbolize_names: true)
+        expect(response_json[:error]).to eq("Customer must exist")
+      end
+
+      it "returns a 400 error if the tea is not found" do
+
+      end
+
+      it "returns a 400 error if any parameter is missing" do
+
+      end
+
+      it "returns a 400 error if the price is not a number" do
+
+      end
+
+      it "returns a 4004 error if the frequency is not valid" do
+
+      end
+
+      it "returns a 400 error if the customer already has a subscription to that tea" do
+
+      end
     end
   end
 end
