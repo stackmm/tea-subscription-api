bf1e8a469540ed9fef70d4008aaf8c86b29e3fb3
Steven Stackhouse <stackmm@gmail.com>
2023-09-13 15:40:54 -0600

    add route for delete sub

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       resources :customers do
         resources :subscriptions, only: [:index]
       end
-      resources :subscriptions, only: [:create]
+      resources :subscriptions, only: [:create, :destroy]
     end
   end
 end
